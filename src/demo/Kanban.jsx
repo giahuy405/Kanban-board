1e2053105a00cde618aa6819232ac9b6b9ca31a4
giahuy405 <pgiahuy653@gmail.com>
2023-03-27 18:45:39 +0700

    fix: key and index

--- src/demo/Kanban.jsx
+++ src/demo/Kanban.jsx
@@ -67,28 +67,27 @@ const Kanban = () => {
                         {_.map(state, (statusTask, index) =>
                             <Droppable droppableId={statusTask.id} key={index}>
                                 {(provided) => {
-                                    return <div className="bg-blue-500" >
-                                        <div className="bg-dark p-1"
-                                            key={index}
-                                            ref={provided.innerRef}
-                                            {...provided.droppableProps}
-                                        >
-                                            <h3 className="text-white">{statusTask.id}</h3>
-                                            {statusTask.items.map((item, index) => {
-                                                return <Draggable key={item.id} index={index} draggableId={item.id}>
-                                                    {(provided) => {
-                                                        return <div
-                                                            ref={provided.innerRef}
-                                                            {...provided.draggableProps}
-                                                            {...provided.dragHandleProps}
-                                                            className="mt-1 py-2 px-14 bg-white text-center">
-                                                            {item.taskName}
-                                                        </div>
-                                                    }}
-                                                </Draggable>
-                                            })}
-                                            {provided.placeholder}
-                                        </div>
+                                    return <div
+                                        key={index}
+                                        ref={provided.innerRef}
+                                        {...provided.droppableProps}
+                                        className="bg-blue-500 h-48 overflow-auto p-1 min-w-[160px] duration-500" >
+                                        <h3 className="text-white">{statusTask.id}</h3>
+                                        {statusTask.items.map((item, index) => {
+                                            return <Draggable key={item.id} index={index} draggableId={item.id}>
+                                                {(provided) => {
+                                                    return <div
+                                                        ref={provided.innerRef}
+                                                        {...provided.draggableProps}
+                                                        {...provided.dragHandleProps}
+                                                        className="mt-1 py-2 px-14 bg-white text-center">
+                                                        {item.taskName}
+                                                    </div>
+                                                }}
+                                            </Draggable>
+                                        })}
+                                        {/* nơi hiển thị chỗ trống khi task dc lấy đi */}
+                                        {provided.placeholder}
                                     </div>
                                 }}
                             </Droppable>)}
